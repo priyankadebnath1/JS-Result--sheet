fd12002459ab7655022e6cf4df22326df3ad7af2
Priyanka Debnath <priyanka.wdebnath@gmail.com>
2021-07-19 21:11:47 +0600

    Result-sheet

---
+++ js/script.js
@@ -0,0 +1,63 @@
+/*if(condition){
+    Result;
+}
+
+else if(condition){
+    Result;
+}
+else{
+    Result;
+}*/
+
+/*var a =50;
+var b =60;
+var c = a+b;
+
+document.write(c);*/
+
+// var a =prompt('Enter your first number');
+// var b =prompt('second number');
+// if(a > b){
+//     document.write('a is big');
+// }
+// else if(a < b){
+//     document.write('b is big');
+// }
+// else{
+//     document.write('condition ')
+// }
+
+
+var a = prompt('Enter your marks');
+// var b = prompt('')
+
+
+if(a >= 90 && a <= 100){
+    document.write('Golden A+');
+}
+else if(a >= 80 && a <= 90){
+    document.write('A+');
+}
+else if(a >= 70 && a <= 79){
+    document.write('A');
+} 
+else if(a >= 60 && a <= 69){
+    document.write('A-');
+}
+else if(a >= 50 && a <= 59){
+    document.write('B+');
+}
+else if(a >= 40 && a <= 49){
+    document.write('B');
+}
+else if(a >= 33 && a <= 39){
+    document.write('c');
+}
+else if(a <= 32){
+    document.write('Fail');
+}
+else{ 
+    document.write('Reject');
+}
+
+
